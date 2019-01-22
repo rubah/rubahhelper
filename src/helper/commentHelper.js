de50c73da3baeca518de9191f93fa8947ca1985a
Averman <averman@gmail.com>
2019-01-22 07:53:58 +0000

    minor fix

--- src/helper/commentHelper.js
+++ src/helper/commentHelper.js
@@ -1,11 +1,20 @@
 module.exports = {
     helperName: 'comment',
     handlebars: function(data) {
-        return '/**\n *\n * '+data.toString().split('\n').join('\n * ')+'\n *\n **/';
+        if(data)
+            return '/**\n *\n * '+data.toString().split('\n').join('\n * ')+'\n *\n **/';
+        else 
+            return '/**\n *\n * \n *\n **/';
     },
     map: function(value) {
-        value = value.substr(10).split('\n * ').join('\n');
-        value = value.substr(0,value.length - 8);
+        value = value.trim().substr(4);
+        const prefix = value.substr(0,value.indexOf('*'));
+        value = value.substr(0,value.length-3);
+        value = value.split(prefix+'*').map(x=>x.substr(1,x.length-2));
+        value.pop();
+        value.shift();
+        value.shift();
+        value = value.join('\n');
         return value;
     }
 }
--- src/helper/jsonHelper.js
+++ src/helper/jsonHelper.js
@@ -1,7 +1,7 @@
 module.exports = {
     helperName: 'json',
     handlebars: function(data) {
-        return JSON.stringify(data);
+        return JSON.stringify(data,null,'\t');
     },
     map: function(value) {
         return JSON.parse(value);
